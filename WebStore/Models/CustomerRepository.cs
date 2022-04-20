27df765930efdf1a66bcb9b31f0f41397716044e
Byron Burks <byron.burks@gmail.com>
2022-04-20 00:28:03 -0500

    add Customer model. add CustomerRepository. add CustomerRepository.Add()

---
+++ WebStore/Models/CustomerRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebStore.Models
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private StoreDbContext context;
+
+        public CustomerRepository(StoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Customer> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> FindBy(Func<Customer, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebStoreTests/CustomerRepositoryTests/AddShould.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using WebStore.Models;
+using Xunit;
+
+namespace WebStoreTests.CustomerRepositoryTests
+{
+    public class AddShould
+    {
+        [Fact]
+        void AttemptToAddNewCustomer()
+        {
+            // arrange
+            var mockSet = new Mock<DbSet<Customer>>();
+
+            var mockContext = new Mock<StoreDbContext>();
+            mockContext.Setup(m => m.Customers).Returns(mockSet.Object);
+
+            // act
+            var uut = new CustomerRepository(mockContext.Object);
+            var customer = new Customer();
+            uut.Add(customer);
+
+            //assert
+            mockSet.Verify(m => m.Add(customer), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
